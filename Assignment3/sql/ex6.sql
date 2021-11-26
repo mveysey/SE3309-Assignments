7ac90f2b567a829fa257be777ba73ef9865a9a3e
mmoftah3 <71080372+mmoftah3@users.noreply.github.com>
2021-11-25 21:48:25 -0500

    Add files via upload
    Exercise 6 (three data modification queries)

---
+++ Assignment3/sql/ex6.sql
@@ -0,0 +1,18 @@
+USE `1200blockbusters+`; 
+
+-- // Based on their first name, if an actor has acted in at least one movie, then they will be set as a popular actor
+UPDATE actor
+SET isPopularActor = 'yes'
+WHERE (actor.fName IN (SELECT ap.fName FROM appearance ap GROUP BY ap.fName HAVING COUNT(ap.title) >= 1));
+
+-- //DELETE
+DELETE FROM appearance WHERE roleName=”BARBIE”;
+
+
+-- // Update roleName in appearance from Anna to Annie  to a new name
+
+UPDATE appearance
+SET 
+	roleName = 'Annie'
+WHERE
+	roleName = 'Anna'
